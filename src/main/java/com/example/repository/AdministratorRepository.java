08d9c4b70385e675a7cd32ecb62784f093c67686
harasawa <h_silvermitt@outlook.jp>
2024-11-26 16:05:25 +0900

    re:SQL Injection

--- src/main/java/com/example/repository/AdministratorRepository.java
+++ src/main/java/com/example/repository/AdministratorRepository.java
@@ -61,10 +61,11 @@ public class AdministratorRepository {
 		String sql = "select id,name,mail_address,password from administrators where mail_address=:mailAddress and password= :password";
 		SqlParameterSource param = new MapSqlParameterSource().addValue("mailAddress", mailAddress).addValue("password", password);
 		List<Administrator> administratorList = template.query(sql, param, ADMINISTRATOR_ROW_MAPPER);
-		if (administratorList.size() == 0) {
-			return null;
-		}
-		return administratorList.get(0);
+		return administratorList.isEmpty() ? null : administratorList.get(0);
+		// if (administratorList.size() == 0) {
+		// 	return null;
+		// }
+		// return administratorList.get(0);
 	}
 
 	/**
@@ -88,7 +89,6 @@ public class AdministratorRepository {
 		String sql = "select id,name,mail_address,password from administrators where mail_address=:mailAddress";
 		SqlParameterSource param = new MapSqlParameterSource().addValue("mailAddress", mailAddress);
 		List<Administrator> administratorList = template.query(sql, param, ADMINISTRATOR_ROW_MAPPER);
-	
 		return administratorList.isEmpty() ? null : administratorList.get(0);
 		
 		// if (administratorList.size() == 0) {
